a9e6f18f4cffa9c1215e4e58715132b89d2c23c5
mattijs <mattijs>
2019-10-22 19:46:26 +0100

    ENH: communicator: testcase. Not working yet.

--- src/OpenFOAM/global/argList/argList.C
+++ src/OpenFOAM/global/argList/argList.C
@@ -166,16 +166,16 @@ Foam::argList::initValidTables::initValidTables()
         true  // advanced option
     );
 
-    argList::addBoolOption
-    (
-        "localWorld",
-        "Use application-local communicator for default communication"
-    );
-    validParOptions.set
-    (
-        "localWorld",
-        "Use application-local communicator for default communication"
-    );
+//    argList::addBoolOption
+//    (
+//        "localWorld",
+//        "Use application-local communicator for default communication"
+//    );
+//    validParOptions.set
+//    (
+//        "localWorld",
+//        "Use application-local communicator for default communication"
+//    );
 
     argList::addOption
     (
@@ -518,7 +518,7 @@ void Foam::argList::noParallel()
     removeOption("roots");
     removeOption("decomposeParDict");
     removeOption("hostRoots");
-    removeOption("localWorld");
+//    removeOption("localWorld");
     removeOption("world");
     validParOptions.clear();
 }
