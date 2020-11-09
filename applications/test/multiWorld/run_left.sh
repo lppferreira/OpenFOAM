70b21659369b89795691f81b9019bb71308c27fe
mattijs <mattijs>
2020-11-09 10:28:04 +0000

    ENH: multiWorld: cleanup

--- applications/test/multiWorld/run_left.sh
+++ applications/test/multiWorld/run_left.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-. /home/mattijs/OpenFOAM/OpenFOAM-plus.feature-localWorld/etc/bashrc 
-cd /home/mattijs/OpenFOAM/OpenFOAM-plus.feature-localWorld/applications/test/multiWorld
-/home/mattijs/OpenFOAM/OpenFOAM-plus.feature-localWorld/platforms/linux64GccDPInt32Debug/bin/laplacianFoam -case ./left -world LEFT 2>&1 | tee run_left.log
+laplacianFoam -case ./left -world LEFT 2>&1 | tee log.run_left
 read dummy
