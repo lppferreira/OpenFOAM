ZhangYanTJU/OpenFOAM-ESI

ENH: buoyancyTurbSource: add a new fvOption
    Applies sources on `k` and/or `omega` to incorporate
    effects of buoyancy in incompressible and compressible flows.

    For incompressible cases:

    Corrections applied to:
    ```
      k         | Turbulent kinetic energy                   [m2/s2]
      omega     | Specific dissipation rate                  [1/s]
    ```

    Required fields:
    ```
      k         | Turbulent kinetic energy                   [m2/s2]
      omega     | Specific dissipation rate                  [1/s]
      alphat    | Kinematic turbulent thermal conductivity   [m2/s]
      T         | Temperature                                [K]
    ```

    For compressible cases:

    Corrections applied to:
    ```
      k         | Turbulent kinetic energy                   [m2/s2]
    ```

    Required fields:
    ```
      k         | Turbulent kinetic energy                   [m2/s2]
      rho       | Fluid density                              [kg/m3]
    ```

    References:
    ```
        Buoyancy modifier for compressible flow cases (tag:DTR):
            Devolder, B., Troch, P., & Rauwoens, P. (2018).
            Performance of a buoyancy-modified k-ω and k-ω
            SST turbulence model for simulating wave breaking
            under regular waves using OpenFOAM.
            Coastal Engineering, 138, 49-65.
            DOI:10.1016/j.coastaleng.2018.04.011
    ```

    Usage
    Minimal example by using `constant/fvOptions`:
    ```
    buoyancyTurbSource1
    {
        // Mandatory entries (unmodifiable)
        type                 buoyancyTurbSource;

        buoyancyTurbSourceCoeffs
        {
            // Mandatory (inherited) entries (unmodifiable)
            selectionMode    all;

            // Optional entries (unmodifiable)
            rho             rho;
            beta            3.3e-03;
        }

        // Optional (inherited) entries
        ...
    }
    ```

src/fvOptions/sources/derived/buoyancyTurbSource/buoyancyTurbSourceTemplates.C
@@ -1,0 +1,68 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2020 OpenCFD Ltd
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "buoyancyTurbSource.H"
+#include "gravityMeshObject.H"
+//#include "volFields.H"
+
+// * * * * * * * * * * * * * * *  Member Functions * * * * * * * * * * * * * //
+
+template<class AlphaFieldType, class RhoFieldType>
+void Foam::fv::buoyancyTurbSource::buoyancyTurbSourceK
+(
+    const AlphaFieldType& alpha,
+    const RhoFieldType& rho,
+    fvMatrix<scalar>& eqn,
+    const label fieldi
+) const
+{
+    const volScalarField& k = eqn.psi();
+    const dimensionedScalar k0(k.dimensions(), SMALL);
+
+    const auto& g = meshObjects::gravity::New(mesh_.time());
+
+    const auto* turbPtr =
+        mesh_.findObject<turbulenceModel>
+        (
+            turbulenceModel::propertiesName
+        );
+    const volScalarField& nut = turbPtr->nut();
+
+    const dictionary& turbDict = turbPtr->coeffDict();
+    const scalar Prt
+    (
+        turbDict.getCheckOrDefault<scalar>("Prt", 0.85, scalarMinMax::ge(SMALL))
+    );
+
+    // (DTR:Eq. 21, buoyancy correction term)
+    const tmp<volScalarField> GbByK((nut/Prt)*(fvc::grad(rho) & g)/max(k, k0));
+
+    eqn -= fvm::Sp(GbByK, k);
+}
+
+
+// ************************************************************************* //

lppferreira/OpenFOAM

    Applies sources on turbulent kinetic energy (i.e. `k`)
    and either turbulent kinetic energy dissipation rate (i.e. `epsilon`)
    or specific dissipation rate (i.e. `omega`) to incorporate effects
    of buoyancy on turbulence in incompressible and compressible flows.
    See buoyancyTurbSource.H for details.

src/fvOptions/sources/derived/buoyancyTurbSource/buoyancyTurbSourceTemplates.C
@@ -1,0 +1,64 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2020 OpenCFD Ltd
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "buoyancyTurbSource.H"
+
+// * * * * * * * * * * * * * * *  Member Functions * * * * * * * * * * * * * //
+
+template<class AlphaFieldType, class RhoFieldType>
+void Foam::fv::buoyancyTurbSource::buoyancyTurbSourceK
+(
+    const AlphaFieldType& alpha,
+    const RhoFieldType& rho,
+    fvMatrix<scalar>& eqn,
+    const label fieldi
+) const
+{
+    const volScalarField& k = eqn.psi();
+    const dimensionedScalar k0(k.dimensions(), SMALL);
+
+    const auto* turbPtr =
+        mesh_.findObject<turbulenceModel>
+        (
+            turbulenceModel::propertiesName
+        );
+    const volScalarField& nut = turbPtr->nut();
+
+    const dictionary& turbDict = turbPtr->coeffDict();
+    const scalar Prt
+    (
+        turbDict.getCheckOrDefault<scalar>("Prt", 0.85, scalarMinMax::ge(SMALL))
+    );
+
+    // (DTR:Eq. 21, buoyancy correction term)
+    const tmp<volScalarField> GbByK((nut/Prt)*(fvc::grad(rho) & g_)/max(k, k0));
+
+    eqn -= fvm::Sp(GbByK, k);
+}
+
+
+// ************************************************************************* //